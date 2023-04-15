f7a0bbc509e2dd829f3867577b9aafae17dbbcae
davidzhuprime <davidzhuprime@yahoo.com>
2023-04-15 18:13:36 +0800

    Initial commit

---
+++ src/main/java/com/example/littleshit/LittleshitApplication.java
@@ -0,0 +1,13 @@
+package com.example.littleshit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleshitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleshitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/littleshit/LittleshitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.littleshit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleshitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
